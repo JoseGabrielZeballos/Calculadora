35045ebc20259770ae2c882f683429ee1bca3e77
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-02-25 20:07:10 -0400

    Se realizo las operaciones de sumar.restar,multiplicar y dividir.

---
+++ Scalculadora/calculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calculadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Add")]
+        public int Add([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa + numb;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public float Multiply([FromHeader] float numa, [FromHeader] float numb)
+        {
+            return numa * numb;
+        }
+        [HttpPost]
+        [Route("Divide")]
+        public float Divide([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa / numb;
+        }
+        [HttpPost]
+        [Route("Subtraction")]
+        public float Subtration([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa - numb;
+        }
+    }
+}
